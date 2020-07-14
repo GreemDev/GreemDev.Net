b6f5a9a5f1be77b42be6828f680589b51fddb175
Evan Husted <gr33m11@gmail.com>
2020-07-13 19:48:58 -0500

    volte project website overhaul

--- GreemDev/Startup.cs
+++ GreemDev/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace GreemDev
 {
@@ -17,10 +18,13 @@ namespace GreemDev
             });
 
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0).AddMvcOptions(options =>
+            {
+                options.EnableEndpointRouting = false;
+            });
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
